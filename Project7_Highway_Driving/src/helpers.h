514c2d250ab597ee1adb12d482f6daf8da9fa907
taijihua <tangjunhua_zju@hotmail.com>
2019-02-26 23:00:39 -0500

    further worked on projectt 7

--- Project7_Highway_Driving/src/helpers.h
+++ Project7_Highway_Driving/src/helpers.h
@@ -5,6 +5,11 @@
 #include <string>
 #include <vector>
 
+#include "Eigen-3.3/Eigen/Core"
+#include "Eigen-3.3/Eigen/QR"
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
 // for convenience
 using std::string;
 using std::vector;
@@ -152,6 +157,49 @@ vector<double> getXY(double s, double d, const vector<double> &maps_s,
   double y = seg_y + d*sin(perp_heading);
 
   return {x,y};
+}
+
+//==========functions above were from Original Udacity code, below are code that i added===========
+vector<double> JMT(vector<double> &start, vector<double> &end, double T) {
+  /**
+   * Calculate the Jerk Minimizing Trajectory that connects the initial state
+   * to the final state in time T.
+   *
+   * @param start - the vehicles start location given as a length three array
+   *   corresponding to initial values of [s, s_dot, s_double_dot]
+   * @param end - the desired end state for vehicle. Like "start" this is a
+   *   length three array.
+   * @param T - The duration, in seconds, over which this maneuver should occur.
+   *
+   * @output an array of length 6, each value corresponding to a coefficent in
+   *   the polynomial:
+   *   s(t) = a_0 + a_1 * t + a_2 * t**2 + a_3 * t**3 + a_4 * t**4 + a_5 * t**5
+   *
+   * EXAMPLE
+   *   > JMT([0, 10, 0], [10, 10, 0], 1)
+   *     [0.0, 10.0, 0.0, 0.0, 0.0, 0.0]
+   */
+  MatrixXd A = MatrixXd(3, 3);
+  A << T*T*T, T*T*T*T, T*T*T*T*T,
+       3*T*T, 4*T*T*T,5*T*T*T*T,
+       6*T, 12*T*T, 20*T*T*T;
+
+  MatrixXd B = MatrixXd(3,1);
+  B << end[0]-(start[0]+start[1]*T+.5*start[2]*T*T),
+       end[1]-(start[1]+start[2]*T),
+       end[2]-start[2];
+
+  MatrixXd Ai = A.inverse();
+
+  MatrixXd C = Ai*B;
+
+  vector <double> result = {start[0], start[1], .5*start[2]};
+
+  for(int i = 0; i < C.size(); ++i) {
+    result.push_back(C.data()[i]);
+  }
+
+  return result;
 }
 
 #endif  // HELPERS_H
--- Project7_Highway_Driving/src/main.cpp
+++ Project7_Highway_Driving/src/main.cpp
@@ -8,7 +8,8 @@
 #include "helpers.h"
 #include "json.hpp"
 
-#include <algorithm> //added by me, to use max()
+#include <algorithm> //added by me, to use max(), min()
+#include "spline.h"  //downloaded spline.h file
 
 // for convenience
 using nlohmann::json;
@@ -99,7 +100,7 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
-          //simple test, move vehicle forward
+          //== simple test, move vehicle forward
 //          double dist_inc = 0.5;
 //          for (int i = 0; i < 50; ++i) {
 //            next_x_vals.push_back(car_x+(dist_inc*i)*cos(deg2rad(car_yaw)));
@@ -108,10 +109,8 @@ int main() {
           double pos_x;
           double pos_y;
           double angle;
-          int path_size = previous_path_x.size();
-          if (path_size>10) //only keep up to 5 previous history points
-        	  path_size=10;
-          for (int i = 0; i < path_size; ++i) {
+          int path_size = std::min(previous_path_x.size(), 5); //only keep up to 5 previous history points
+          for(int i = 0; i < path_size; ++i) {
             next_x_vals.push_back(previous_path_x[i]);
             next_y_vals.push_back(previous_path_y[i]);
           }
@@ -144,15 +143,137 @@ int main() {
           std::cout<<"current pos_x, pos_y: "<<pos_x<<" "<<pos_y<<std::endl;
           std::cout<<"current pos_s: "<<pos_s<<std::endl;
 
-          double s_inc = 0.44;  // 50mph -> 22.352 m/s -> 0.447 m/0.02s
-          for (int i = 0; i < 50-path_size; ++i) {
+          double ref_vel = 49.5; //mph
+          double delta_T = 0.02; //0.02 second per path point
+          double lane = 1; //lane = 0, 1, 2
+          double s_inc= ref_vel*1600.0/3600.0*deta_T; // 50mph -> 22.352 m/s -> 0.447 m/0.02s
+          double target_s;
+          double target_d;
+          int nPts = 50-path_size;  // append n points from (pos_s, pos_d) to (target_s, target_d)
+//          target_s = pos_s+nPts*s_inc;
+//          target_d = pos_d; //stay in lane
+
+          //check other vehicles
+          //std::cout<<"set target_s to "<<target_s<<std::endl;
+
+          for(int i= 0; i< sensor_fusion.size(); i++)
+          {
+            //std::cout<<sensor_fusion[i]<<", ";
+        	  auto obj = sensor_fusion[i];
+//        	  if(obj["s"]<target_s && obj["s"]>=pos_s && fabs(obj["d"]-target_d)<2)
+//        		  target_s = obj["s"];
+        	  double obj_s = obj[5];
+        	  double obj_d = obj[6];
+
+        	  std::cout<<" obj_s = "<<obj_s<<std::endl;
+
+        	  if(obj_s<target_s && obj_s>=pos_s && fabs(obj_d-target_d)<2)
+        	  {
+        	  	  target_s = obj_s;
+        	  	  std::cout<<"!! Update target_s to "<<target_s<<std::endl;
+        	  }
+          }
+
+//          // Generate trajectory based on (pos_s, pos_d) and (obj_s, obj_d) using polynomial
+//          vector<double> s_coeff = JMT(s_start, s_end);
+//          vector<double> d_coeff = JMT(d_start, d_end, )
+
+          //generate trajectory based on spline function
+          vector<double> ptsx;
+          vector<double> ptsy;
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+          //generate points to make sure path is tangent to current yaw
+          if(prev_size<2) //if previous size is almost empty, use the car as starting reference
+          {
+        	  double prev_car_x = car_x-cos(car_yaw);
+        	  double prev_car_y = car_y-sin(car_yaw);
+        	  ptsx.push_back(pre_car_x);
+        	  ptsx.push_back(car_x);
+
+        	  ptsy.push_back(prev_car_y);
+        	  ptsy.push_back(car_y);
+
+          }
+          else
+          {
+        	  ref_x = previous_path_x[prev_size-1];
+        	  ref_y = previous_path_y[prev_size-1];
+        	  double ref_x_prev = previous_path_x[prev_size-2];
+        	  double ref_y_prev = previous_path_y[prev_size-2];
+        	  ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+        	  ptsx.push_back(ref_x_prev);
+        	  ptsx.push_back(ref_x);
+
+        	  ptsy.push_back(ref_y_prev);
+        	  ptsy.push_back(ref_y);
+          }
+
+          vector<double> next_wp0 = getXY(car_s+30, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s+60, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s+90, (2+4*lane), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
+
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+
+          for(int i = 0; i<ptsx.size(); i++)
+          {
+        	  // shift car reference angle to 0 degrees
+        	  double shift_x = ptsx[i]-ref_x;
+        	  double shift_y = ptsy[i]-ref_y;
+        	  ptsx[i] = (shift_x*cos(0-ref_yaw)-shift_y*sin(0-ref_yaw));
+        	  ptsy[i] = (shift_x*sin(0-ref_yaw)+shift_y*cos(0-ref_yaw));
+          }
+          //create spline
+          tk::spline s;
+          s.set_points(ptsx, ptsy);
+          vector<double> next_x_vals;
+          vector<double> next_y_vals;
+          for(int i=0;i<previous_path_x.size(); i++)
+          {
+        	  next_x_vals.push_back(previous_path_x[i]);
+        	  next_y_vals.push_back(previous_path_y[i]);
+          }
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt(target_x*target_x+target_y*target_y);
+          double x_add_on = 0;
+          // fill up the rest of path planner
+          for(int i=1; i<=50-previous_path_x.size(); i++)
+          {
+        	  double N = (target_dist/(.02*ref_vel/2.24));
+        	  double x_point = x_add_on+(target_x)/N;
+        	  double y_point = s(x_point);
+        	  x_add_on = x_point;
+        	  double x_ref = x_point;
+        	  double y_ref = y_point;
+        	  x_point = (x-ref*cos(ref_yaw)-y_ref*sin(ref_yaw));
+        	  y_point = (y_ref*sin(ref_yaw)+y_ref*cos(ref_yaw));
+        	  x_point += ref_x;
+        	  next_x_vals.push_back(x_point);
+        	  next_y_vals.push_back(y_point);
+          }
+          s_inc = (target_s-pos_s)/nPts;
+          for (int i = 0; i < nPts; ++i) {
         	pos_s += s_inc;
-        	pos_d = pos_d;
         	vector<double> xy = getXY(pos_s, pos_d, map_waypoints_s,  map_waypoints_x, map_waypoints_y);
             next_x_vals.push_back(xy[0]);
             next_y_vals.push_back(xy[1]);
           }
-          std::cout<<"target pos_s: "<<pos_s<<std::endl;
+         // show sensor fusion data: [id, x, y, vx, vy, s, d]
+//          std::cout<<"sensor_fusion data: ";
+//          for(int i= 0; i< sensor_fusion.size(); i++)
+//          {
+//        	  std::cout<<sensor_fusion[i]<<", ";
+//          }
+
+          //std::cout<<"target pos_s: "<<pos_s<<std::endl;
 //          std::cout<<"Planned Path:===== ";
 //          for(int i=0;i<next_x_vals.size();i++)
 //        	  std::cout<<next_x_vals[i]<<" "<<next_y_vals[i]<<std::endl;
